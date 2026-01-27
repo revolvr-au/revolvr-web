8b8813d3cc58c0d67a9dc599426d8e4eca51ab31
revolvr-au <revolvr.au@gmail.com>
2026-01-27 00:37:43 +0000

    Add Creator Terms to profile menu

--- src/app/u/[email]/ProfileClient.tsx
+++ src/app/u/[email]/ProfileClient.tsx
@@ -364,6 +364,14 @@ export default function ProfileClient(props: ProfileClientProps) {
               >
                 Terms &amp; Conditions
               </Link>
+                            <Link
+                href="/creator/terms"
+                className="block rounded-xl px-3 py-2 text-sm text-white/80 hover:bg-white/10"
+                onClick={() => setMenuOpen(false)}
+              >
+                Creator Terms
+              </Link>
+
               <Link
                 href="/privacy"
                 className="block rounded-xl px-3 py-2 text-sm text-white/80 hover:bg-white/10"
