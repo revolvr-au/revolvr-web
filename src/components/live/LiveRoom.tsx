7d473438a1af6ababdea07df2740775485653a3f
revolvr-au <revolvr.au@gmail.com>
2026-02-24 03:09:52 +0000

    fix(live): mark LiveRoom as client component for interactive LIVE overlay

--- src/components/live/LiveRoom.tsx
+++ src/components/live/LiveRoom.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import VideoCanvas from "./VideoCanvas";
 import TopBar from "./TopBar";
 import CommentRail from "./CommentRail";
@@ -8,10 +10,7 @@ export default function LiveRoom() {
   return (
     <div className="relative h-screen w-screen overflow-hidden bg-black">
       <VideoCanvas />
-
-      {/* Gradient Edge */}
       <div className="pointer-events-none absolute inset-0 bg-gradient-to-t from-black/60 via-transparent to-black/40 z-5" />
-
       <TopBar />
       <CommentRail />
       <HeartStream />
