8e2defdf0fc565b546d4c9be9c9c2b11ceaeb791
revolvr-au <revolvr.au@gmail.com>
2026-02-27 03:06:17 +0000

    fix: remove isLiveRoute logic fully from LayoutShell to resolve build error

--- src/components/navigation/LayoutShell.tsx
+++ src/components/navigation/LayoutShell.tsx
@@ -1,27 +1,9 @@
 "use client";
 
-import { usePathname } from "next/navigation";
-import BottomNav from "./BottomNav";
-
 export default function LayoutShell({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const pathname = usePathname();
-  
-
-  return (
-    <>
-      <div
-        style={{
-          paddingBottom: isLiveRoute
-            ? 0
-            : "calc(80px + env(safe-area-inset-bottom))",
-        }}
-      >
-        {children}
-      </div>
-    </>
-  );
+  return <>{children}</>;
 }
