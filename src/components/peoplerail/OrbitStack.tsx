f580d57be365e716e4c0e4184ea34781ec4356d3
revolvr-au <revolvr.au@gmail.com>
2026-03-13 03:37:04 +0000

    fix(orbit): render avatar image in OrbitAvatar component instead of placeholder circle

--- src/components/peoplerail/OrbitStack.tsx
+++ src/components/peoplerail/OrbitStack.tsx
@@ -1,12 +1,25 @@
 import OrbitAvatar from "./OrbitAvatar"
 
-export default function OrbitStack({ users }) {
-
+export default function OrbitAvatar({ user }) {
   return (
-    <div className="orbit-stack">
-      {users.map((user) => (
-        <OrbitAvatar key={user.id} user={user} />
-      ))}
+    <div
+      style={{
+        width: 54,
+        height: 54,
+        borderRadius: "50%",
+        overflow: "hidden",
+        border: user.live ? "2px solid #ff2d55" : "2px solid #666"
+      }}
+    >
+      <img
+        src={user.avatar}
+        alt=""
+        style={{
+          width: "100%",
+          height: "100%",
+          objectFit: "cover"
+        }}
+      />
     </div>
   )
 }
