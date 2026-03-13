8e5a2db0bf6f06fc595d96e00047832d8b228a7c
revolvr-au <revolvr.au@gmail.com>
2026-03-13 02:37:43 +0000

    fix(peoplerail): mark hook as client component

--- src/hook/usePeopleRail.ts
+++ src/hook/usePeopleRail.ts
@@ -1,3 +1,6 @@
+"use client";
+
+
 import { useEffect, useState } from "react"
 import { fetchOrbitRail } from "../services/orbitService"
 import { fetchPresenceStates } from "../services/presenceService"
