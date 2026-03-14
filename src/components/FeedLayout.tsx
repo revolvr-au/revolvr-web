deef69d254dba276d17376df838741cae81dab0b
revolvr-au <revolvr.au@gmail.com>
2026-03-14 23:30:22 +0000

    Fix FeedLayout so feed expands full width and remove layout constraint from PeopleRail container

--- src/components/FeedLayout.tsx
+++ src/components/FeedLayout.tsx
@@ -83,28 +83,23 @@ export default function FeedLayout({
       )}
 
       <main className="flex-1 w-full pb-20">
- <div
-  style={{
-    display: "flex",
-    width: "100%"
-  }}
->
+  <div style={{ display: "flex", width: "100%" }}>
 
-  {peopleOpen && (
-    <PeopleRail
-      open={peopleOpen}
-      userId="test-user"
-      activePost={activePost}
-      users={railUsers}
-      onSelectCreator={onSelectCreator}
-    />
-  )}
+    {peopleOpen && (
+      <PeopleRail
+        open={peopleOpen}
+        userId="test-user"
+        activePost={activePost}
+        users={railUsers}
+        onSelectCreator={onSelectCreator}
+      />
+    )}
 
-  <div style={{ flex: 1 }}>
-    {children}
+    <div style={{ flex: 1, width: "100%" }}>
+      {children}
+    </div>
+
   </div>
-
-</div>
 </main>
       {!isLive && <BottomBar />}
 
